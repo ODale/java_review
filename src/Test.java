868bdb32f618db0f8f28387890cdcf5ec5b45f87
O'Dale Cotterell <ocotterell@gmail.com>
2017-03-24 19:52:06 -0400

    basic-arithmetic
    input, output, and basic calculations

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Test{
 	public static void main(String[] args){
-		System.out.println("Hello Java");
+		
+		//instantiate Scanner object to read keyboard inputs
+		Scanner input = new Scanner(System.in);
+		
+		//variable declaration
+		int numberOne, numberTwo, numberThree, sum = 0, product = 0;
+		double average = 0.0;
+		
+		//get user inputs
+		System.out.println("Enter first number: ");
+		numberOne = input.nextInt();
+		System.out.println("Enter sencond number: ");
+		numberTwo = input.nextInt();
+		System.out.println("Enter third number: ");
+		numberThree = input.nextInt();
+		
+		//close scanner
+		input.close();
+		
+		//calculations
+		sum = numberOne + numberTwo + numberThree;
+		product = numberOne * numberTwo * numberThree;
+		average = sum/3.0;
+		
+		//output the results to the user
+		System.out.println("The sum is: " + sum);
+		System.out.println("The product is: " + product);
+		System.out.println("The average is: " + average);
 	}
 }
